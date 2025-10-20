d3db64446a122916333e47d1bb8f7602a21576ad
Will Forkes <35146567+WillForkes@users.noreply.github.com>
2025-10-20 21:13:34 +0100

    Initial Commit

---
+++ Drift/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Drift
+//
+//  Created by William Forkes on 20/10/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
